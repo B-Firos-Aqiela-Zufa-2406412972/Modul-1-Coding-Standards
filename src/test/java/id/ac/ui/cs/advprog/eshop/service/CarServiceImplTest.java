c3ba2857a5c29163424cba948e85c6df7060f655
firoszfa <firoszufa20@gmail.com>
2026-03-03 17:42:37 +0700

    added CarService test

---
+++ src/test/java/id/ac/ui/cs/advprog/eshop/service/CarServiceImplTest.java
@@ -0,0 +1,34 @@
+package id.ac.ui.cs.advprog.eshop.service;
+
+import id.ac.ui.cs.advprog.eshop.model.Car;
+import id.ac.ui.cs.advprog.eshop.repository.CarRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class CarServiceImplTest {
+
+    @Mock
+    private CarRepository carRepository;
+
+    @InjectMocks
+    private CarServiceImpl carService;
+
+    @Test
+    void testUpdateCarService() {
+        Car car = new Car();
+        car.setId("1");
+        car.setName("Tesla");
+
+        carService.update(car);
+
+        // Verifies the service passes the correct ID and Object to the repository
+        verify(carRepository, times(1)).update(eq("1"), any(Car.class));
+    }
+}
