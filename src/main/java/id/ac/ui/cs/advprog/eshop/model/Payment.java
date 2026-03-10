95a8f544eb01f59938e0fba51ecf0ca54c1947f5
firoszfa <firoszufa20@gmail.com>
2026-03-10 16:39:45 +0700

    [GREEN] Implement Payment class

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
@@ -12,8 +12,16 @@ public class Payment {
     Order order;
 
     public Payment(String id, String method, Map<String, String> paymentData, Order order) {
+        if (paymentData == null || paymentData.isEmpty()) {
+            throw new IllegalArgumentException("Payment data cannot be empty");
+        }
+        this.id = id;
+        this.method = method;
+        this.paymentData = paymentData;
+        this.order = order;
     }
 
     public void setStatus(String status) {
+        this.status = status;
     }
 }
